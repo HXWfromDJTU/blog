5ddf3f7cca8fdfdc226dc1782a0893e4c23cd0c6
SwainWong <404813471@qq.com>
2019-01-12 17:33:32 +0800

    深拷贝

--- algorithm/deepCopy.js
+++ algorithm/deepCopy.js
@@ -2,8 +2,8 @@
 let isComplex = data =>{
    let str = Object.prototype.toString.call(data);
    let dict = {
-       '[Object Array]':true,
-       '[Object Object]':true
+       '[object Array]':true,
+       '[object Object]':true
    };
    if(dict[str]){
        return true;
@@ -11,106 +11,61 @@ let isComplex = data =>{
        return false;
    }
 }
-/**
- * 
- * @param {*} obj1 拷贝目标输出对象
- * @param {*} obj2 拷贝模板对象
- */
-let deepCopy = (obj1,obj2) =>{
-    for(let attr in obj2){ // 注意此处不可以使用 for of遍历，因JS对象并没有遍历器接口 
-        if(isComplex(obj2[attr])){
-           deepCopy(obj2);
-        }else{
-             obj1[attr] = obj2[attr]
-        }
-    }
-    return obj1;
-}
+
 
 /**
  *  其他疑问  
  * 1 循环引用  
  * 2 引用丢失   
  * 3 使用递归，调用栈溢出  
- *  */
+ *  
+ */
 
-// let template = {
-//     abc:{
-//         qqq:456
-//     }
-// }
-// template.abc.qqq = template;
 
-// let b = deepCopy({},template);
-// console.log(b);
-
-deepCopy = function(target,source){
+let deepCopy = function(target,source){
     let stack = [];// 使用一个数组作为调用栈，系统的调用栈，避免递归栈限制
     // 使用一个WeakMap存储已经拷贝过的对象
-    let currentSet = new WeakMap();
+    let currentMap = new Map();
     for(let attr in source){
         if(isComplex(source[attr])){
-            let hasAlready = currentSet.has(source[attr]); // 判断之前是否已经拷贝过一次 
+            let hasAlready = currentMap.has(source[attr]); // 判断之前是否已经拷贝过一次 
             if(hasAlready){
-                target[arrt] = currentSet.get(source[attr]); // 若已经拷贝过，则将原来拷贝的一份取出来(也包括嵌套的问题，因为不再取去拷贝了)
-            }else{
-                stack.push(source[attr]); // 收纳每一个需要遍历的数据 （相当于收集了第一层的入口）
+                target[attr] = source[attr]; // 若已经拷贝过，则将原来拷贝的一份取出来(也包括嵌套的问题，因为不再取去拷贝了)  
+            }else{  
+                let result = deepCopy(source[attr]); 
+                currentMap.set(result,result); // 存储
+                target[attr] = result; 
             }
         }else{
-            target[attr] = source;
+            target[attr] = source[attr];
         }
     }
-
-    // while(){
-
-    // }
-
-
     return target;
 }
 
+let o1 = {
+    key:70890
+}
+let o2 = {
+    abc:[345,456,123],
+    qqq:{
+        asd:123
+    },
+    test:'一般元素',
+    fun:function(){
+        console.log(this.test);
+    },
+    dupTest1:o1,
+    dupTest2:o1
+}
+
+let target = deepCopy({},o2);
+
+console.log(target)
+target.dupTest1.key = 123;
+console.log(target)
 
 
-
-
-
-
-
-
-
-
-
-// let o2 = {
-//     abc:[345,456,123],
-//     qqq:{
-//         asd:123
-//     },
-//     test:'一般元素',
-//     fun:function(){
-//         console.log(this.test);
-//     }
-// }
-
-// let target = deepCopy({},o2);
-
-// console.log('刚拷贝好对象：',target);
-
-// target.abc.push(456);
-// target.qqq.asd = 456;
-// target.fun = function(){
-//     console.log('函数被我被修改了')
-// }
-
-// console.log('修改后的对象：',target);
-
-
-// console.log('原模板：',o2);
-// o2.fun();
-
-
-// let shallowCopy = (target,tempalte) =>{
-    
-// }
 
 
 // module.exports = {
