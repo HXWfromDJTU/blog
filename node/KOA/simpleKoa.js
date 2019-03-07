5094f243b38393692b3a12b45304bc6bd41a9d75
SwainWong <404813471@qq.com>
2019-03-07 18:13:07 +0800

    koa跑通洋葱模型

--- node/KOA/simpleKoa.js
+++ node/KOA/simpleKoa.js
@@ -8,9 +8,10 @@ class Koa {
         this.request = Object.create(null);
         this.response = Object.create(null);
         // 初始化的时候创建一个http服务器
-        this.server = Koa.http.createServer(ctx => {
-            console.log('被连接上了.....')
-            this._next(ctx);
+        this.server = Koa.http.createServer(rawCtx => {
+            this.createContext(rawCtx);//使用这个方法处理 rawCtx
+            //console.log('被连接上了.....')
+            this._next(this.context); // 启动链式调用,传入上下文对象作为参数        
         });
     }
     /**
@@ -24,7 +25,7 @@ class Koa {
      * 当请求到来的时候，我们清空任务队列中的内容
      */
     _next(ctx) {
-        // 取出中间件
+        // 取出中间件    
         const callbacks = this.middlewares;
         if (callbacks.length === 0) return;// 执行栈清空，停止执行
         let work = callbacks.shift(); // 取出第一个处理过程
@@ -35,14 +36,31 @@ class Koa {
      * @param {*} req 请求对象
      * @param {*} res 返回对象
      */
-    createContext(req, res) {
-        let ctx = this.context;
-        // 使用req和res来装饰 ctx 对象
-        ctx.request = this.request;
-        ctx.response = this.response;
-        ctx.req = this.request.req = req;
-        ctx.res = this.response.res = res;
-        return ctx;
+    createContext(rawCtx) {
+        let { rawHeaders, url, method, statusCode, statusMessage, Socket } = rawCtx;
+        let request = {
+            method,
+            url,
+            header: rawHeaders
+        }
+        let response = {
+            status: statusCode,
+            message: statusMessage,
+            header: null
+        }
+        let res = {};
+        let req = {};
+        // 将组装好的对象挂载到 context 属性上。           
+        this.context = {
+            request,
+            response,
+            app: {},
+            originUrl: '',
+            res,
+            req,
+            socket: Socket
+        }
+        return this.context;
     }
     /**
      * 监听端口号
--- node/KOA/test.js
+++ node/KOA/test.js
@@ -5,22 +5,21 @@ let koa = new Koa();
 
 
 koa.use(async (ctx, next) => {
-    console.log('第一个处理过程');
+    //console.log(ctx);
     // 处理后的ctx，继续传送给下一步
     await next(ctx);
+    console.log(ctx.body)
 });
 
 koa.use((ctx, next) => {
-    console.log('进入第二个处理过程，但需要等待');
-    setTimeout(data => {
-        console.log(`正式开始第二个过程`)
-        next(ctx);
-    })
-});
-
-koa.use((ctx, next) => {
-    console.log('最后一个处理过程');
+    ctx.abc = 123;
     next(ctx);
+    console.log('第二个过程的后续')
+});
+
+// 模拟最终处理过程
+koa.use((ctx, next) => {
+    ctx.body = "Hello World!"
 });
 
 
