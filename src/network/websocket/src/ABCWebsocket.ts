f0458a12f2fce7e29dbfe092d3f5960fd9e9ae00
swain wong <swainwong93@gmail.com>
2020-06-15 13:35:43 +0800

    feat: websocket 请求库封装

---
+++ src/network/websocket/src/ABCWebsocket.ts
@@ -0,0 +1,28 @@
+import * as EventEmitter from 'eventemitter3'
+import uniqueId from 'lodash-es/uniqueId'
+const pkg = require('../package.json')
+const JSON_RPC_VERSION = '2.0'
+
+interface IOption {
+  url: string
+}
+
+export class ABCWebsocket extends Event {
+  protected _serverUrl: string
+  protected _channel: WebSocket
+
+  constructor (option: IOption) {
+    super()
+    this._serverUrl = option.url
+    this._channel = new WebSocket(this._serverUrl)
+  }
+
+  request (data: any): Promise<any> {
+    return new Promise((resolve, reject): void => {
+      data = Object.assign(data, {
+        id: uniqueId(pkg.name + '-'),
+        jsonrpc: JSON_RPC_VERSION
+      })
+    })
+  }
+}
